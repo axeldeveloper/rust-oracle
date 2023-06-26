207234e1de62e60223cd045b7942e053192b16a5
Axel Alexander <axel_nomre@hotmail.com>
2023-06-26 08:37:53 -0400

    refactor(minio) add mino

--- src/services/services_minio.rs
+++ src/services/services_minio.rs
@@ -1,31 +1,68 @@
-use minio_rsc::client::Minio;
-use minio_rsc::provider::StaticProvider;
-use minio_rsc::errors::{Result};
-use dotenv::dotenv;
+use std::result::Result;
+use std::error::Error;
+use minio_rsc::types::{Bucket};
+use minio_rsc::types::args::{ObjectArgs, ListObjectsArgs};
+use minio_rsc::errors::Result as MResult;
+use minio_rsc::types::response::ListBucketResult;
+use reqwest::Response;
+use crate::common::get_client_minio;
 
 
-pub fn get_test_minio() -> Minio  {
-    dotenv().ok();
-    let _minio_url: String = std::env::var("MINIO_URL").expect("DB_DSN must be set.");
-    let minio_key: String = std::env::var("MINIO_KEY").expect("DB_DSN must be set.");
-    let minio_secret: String = std::env::var("MINIO_SECRET").expect("DB_DSN must be set.");
-    let provider = StaticProvider::new(minio_key, minio_secret, None);
-
-    let minio = Minio::builder()
-        .host("minio.sad.ms.gov.br")
-        .provider(provider)
-        .secure(false)
-        //.build()
-        .unwrap();
-
-        minio
-
-        //build(self) -> std::result::Result<Minio, ValueError> 
-}
+//use minio_rsc::errors;
+//use crate::errors::{Error, Result, XmlError} as a,b,c ;
+//use minio_rsc::client::Minio;
+//use minio_rsc::provider::StaticProvider;
+//use minio_rsc::errors::{Result, XmlError, MinioError};
 
 
-pub async fn list_buckets() -> Result<()> {
-    let minio = get_test_minio();
+pub async fn async_buckets_print() {
+    println!("------------------------------------------------------------------------------");
+    let minio = get_client_minio();
     let (buckets, owner) = minio.list_buckets().await.unwrap();
-    Ok(())
+    println!(" Service Owner {}", owner.display_name );
+    for obj in buckets {
+        println!("{}", obj.creation_date );
+        println!("{}", obj.name );
+    }
 }
+
+pub async fn list_buckets() -> Result<Vec<Bucket>, Box<dyn Error>>  {
+    println!("------------------------------------------------------------------------------");
+    let minio = get_client_minio();
+    let (buckets, _owner) = minio.list_buckets().await.unwrap();
+    Ok(buckets)
+}
+
+pub async fn get_object() -> MResult<Response>  {
+    println!("------------------------------------------------------------------------------"); 
+    let minio = get_client_minio();
+    let response: Response = minio.get_object(
+        ObjectArgs::new("pasta-virtual", "mpdf.pdf")
+            //.version_id(Some("cdabf31a-9752-4265-b137-6b3961fbaf9b".to_string()))
+        ).await?;
+
+    Ok(response)
+}
+
+
+
+/*
+
+
+pub async fn list_objects() -> ListBucketResult {
+    println!("------------------------------------------------------------------------------");
+    let minio = get_client_minio();
+
+    let args = ListObjectsArgs::new("pasta-virtual")
+    .max_keys(10);
+
+    Ok(minio.list_objects(args).await);
+}
+
+
+
+*/
+
+
+
+//fn make_error() -> Result<(), String> { Err("error".to_string()) }
