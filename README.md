2236fc723bea4c049210c5119b050e7cd10581b7
axel <axel_nomore@hotmail.com>
2023-06-26 07:06:55 -0400

    feature add minio

--- README.md
+++ README.md
@@ -7,4 +7,7 @@ This installed Rust 1.64.0 on my LiveLab container.
 rustc -V
 cargo -V
 
-# oracle = { version = "0.5", features = ["chrono"] }
+# Dependencies
+oracle = { version = "0.5", features = ["chrono"] }
+
+https://github.com/minio/minio/issues/14498
--- src/main.rs
+++ src/main.rs
@@ -18,33 +18,80 @@ table! {
 // https://github.com/kubo/rust-oracle
 
 use std::env;
-use oracle::{Connection, Result, Version};
+use dotenv::dotenv;
+//use minio_rsc::errors::MinioError;
+use oracle::{Connection, Result};
+use tokio;
 
-fn main() -> Result<()> {
-
-
-
-    let key = "DB_URL";
-    match env::var_os(key) {
-        Some(val) => println!("{key}: {val:?}"),
-        None => println!("{key} is not defined in the environment.")
-    }
+#[path = "minio/services_minio.rs"] mod services_minio;
+#[path = "minio/common.rs"] mod common;
+use common::get_client_minio;
 
 
+#[tokio::main]
+async fn main() -> Result<()> {
 
-    // "oracle://user:secret@127.0.0.1/MY_DB"
-    // let conn = Connection::connect("system", "oracle", svc)?;
+    let minio = get_client_minio();
+
+    services_minio::async_buckets_print().await;
+
+    println!("{:?}", minio.list_buckets().await);
+    println!("====== begin test tagging");
+   
+    let r7 = services_minio::list_buckets().await;
+    match r7 {
+        Ok(bucket) => println!("Bucket is {:?}!!", bucket),
+        
+        Err(e) => eprintln!("Oh noes, we don't know which era we're in! :( \n  {}", e),
+      }
+
+   
+    println!("====== begin oracle ");
+
+    let result = ler_ora();
+    
+    let response = match result {
+        Ok(res) => println!("res is {:?}!!", res) ,
+        Err(err) => return Err(err),
+    };
+
+    println!("to response {:?}!!", response);
+
+
+    let r8 = services_minio::get_object().await;
+
+    let resp = match r8 {
+        Ok(res) => println!("res is {:?}!!", res.url().path()) ,
+        
+        //Err(error) => return S3Error(error) 
+        
+        Err(error) => {
+            panic!("=> There was a problem opening the file: {:?}", error)
+        },
+    };
+
+    println!("to resp {:?}!!", resp);
+
+    Ok(())
+
+}
+
+
+
+fn ler_ora() -> Result<()> {
+    dotenv().ok(); // Load the .env file
+    println!("Oarecle data");
+  
+    let db_url = env::var("DB_URL").expect("You've not set the DB_URL");
+    let db_user = env::var("DB_USER").expect("You've not set the DB_USER");
+    let db_pwd = env::var("DB_PWD").expect("You've not set the DB_PWD");
+
+
     // Connect to a database.
-    let conn = Connection::connect("system", "oracle", "localhost:49161/XE")?;
-
-    //let sql = "select * from COUNTRIES where COUNTRY_ID = :1";
-
-
+    let conn = Connection::connect(db_user, db_pwd, db_url)?;
     let (server_ver, banner) = conn.server_version()?;
-
     println!("\nDatabase Server Version: {}", server_ver);
     println!("\nServer Banner: {}\n", banner);
-
     let sql = "select * from HR.COUNTRIES ";
     let mut stmt = conn.statement(sql).build()?;
     let rows = stmt.query(&[])?;
